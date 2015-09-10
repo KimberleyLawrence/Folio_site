966d48e90c2970faa0192be72dce1db7a549ddb6
KimberleyLawrence <kimberleynedic@gmail.com>
2015-09-09 23:30:41 -0700

    sorting out the light box
    new trick in index.html;
    href=“javascript:void(0)” to allow the text to be clicked on, but not
    allow the box to bounce to the top of the page

--- js/lightbox_index.js
+++ js/lightbox_index.js
@@ -1,25 +1,23 @@
-//when the document is ready, start function
-$(document).ready(function(){
+  //when the document is ready, start function
+  $(document).ready(function(){
   //when class="lightbox" is clicked, start function
-  $('.lightbox').click(function(){
+    $('.lightbox').click(function(){
     //images with class="lazy" are loaded/triggered when 'openlightbox' is called (this is the other end metephoric phone call)
-    $("img.lazy").trigger('openlightbox');
+      $('img.lazy').trigger('openlightbox');
     //class="backdrop" and "box" are shown/appear when ".lightbox" is clicked
     $('.backdrop, .box').show();
-});
-// pretty much the same as above, but this time the box and backdrop close when the ".close" class is clicked.
-$('.close').click(function(){
-  $('.backdrop, .box').hide();
   });
-//  $('.backdrop').click(function(){
-  //  $('.backdrop, .box').toggle(false);
-});
+  // pretty much the same as above, but this time the box and backdrop close when the ".close" class is clicked.
+  $('.close').click(function(){
+    $('.backdrop, .box').hide();
+    });
+  });
 
-//when window is loaded
-$(window).bind("load", function() {
+  //when window is loaded
+  $(window).bind("load", function() {
   //image with class 'lazy', call lazyload plugin (see html script for details)
-  $('img.lazy').lazyload({
+    $('img.lazy').lazyload({
     // listener - listens for 'openlightbox' to be called in the code (this is one end of the metephoric phonecall)
     event: 'openlightbox'
+    });
   });
-});
