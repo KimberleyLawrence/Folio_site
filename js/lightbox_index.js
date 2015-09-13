d224fbb9b16b62206b5904666e411536c8b007bc
KimberleyLawrence <kimberleynedic@gmail.com>
2015-09-12 23:16:20 -0700

    Fixing all the errors
    Fixed the double ups of names, image names and alts for the light box
    images.
    Fixed all the issues with the double box opening with jQuery. So now
    one box can open and closed without effecting others.

--- js/lightbox_index.js
+++ js/lightbox_index.js
@@ -5,11 +5,26 @@
     //images with class="lazy" are loaded/triggered when 'openlightbox' is called (this is the other end metephoric phone call)
       $('img.lazy').trigger('openlightbox');
     //class="backdrop" and "box" are shown/appear when ".lightbox" is clicked
-    $('.backdrop, .box').show();
+    // $('.backdrop, .box').show();
+    //console.log($(this));
+    //console.log($(this).attr("data-id"));
+    //      .myclass[reference="12345"]
+    var box_lookup = '.box[data-id="' + $(this).attr("data-id") + '"]';
+    console.log(box_lookup);
+    $(box_lookup).show();
+    var backdrop_lookup = '.backdrop[data-id="' + $(this).attr("data-id") + '"]';
+    console.log(backdrop_lookup);
+    $(backdrop_lookup).show();
   });
   // pretty much the same as above, but this time the box and backdrop close when the ".close" class is clicked.
   $('.close').click(function(){
-    $('.backdrop, .box').hide();
+  //  $('.backdrop, .box').hide();
+    var box_lookup = '.box[data-id="' + $(this).attr("data-id") + '"]';
+    console.log(box_lookup);
+    $(box_lookup).hide();
+    var backdrop_lookup = '.backdrop[data-id="' + $(this).attr("data-id") + '"]';
+    console.log(backdrop_lookup);
+    $(backdrop_lookup).hide();
     });
   });
 
